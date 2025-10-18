c5896e170c018fdb73ddecfa633dc47f598ceb0e
sanya90smokex <96256075+sanya90smokex@users.noreply.github.com>
2025-10-18 16:48:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-#!/usr/bin/env bash
-set -e
-echo "Запуск start.sh"
-echo "Текущая директория:"; pwd
-echo "Файлы:"; ls -la
-
-python -u server.py &
-python -u bot.py
+#!/bin/bash
+echo "Запуск Telegram-бота..."
+python bot.py
