39b66c1457291274970385455baddfce93278c03
sanya90smokex <96256075+sanya90smokex@users.noreply.github.com>
2025-10-18 15:25:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,9 +5,7 @@ app = Flask(name)
 
 @app.get("/")
 def ok():
-    # healthcheck для Railway
     return "OK"
 
 if name == "main":
-    # Railway сам передает порт через переменную PORT
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
